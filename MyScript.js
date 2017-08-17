71b5e9920129a309946048248ea330387936c50b
Rahul Teja Teepalapudi <me.rahulteja@gmail.com>
2017-08-17 17:21:25 +0530

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -1,4 +1,10 @@
+//============================//
+// Author : Rahul Teja T      //
+// Created Date : 08/17/2017  //              
+//============================//
 
+
+//IDENTIFYING THE CLICK EVENT ON SELECTION OF RECORD FROM LIST-A
 $('.li_grp_A').off('click', '.li_grp_item').on('click', '.li_grp_item', function (event) {
     var $this = $(this);
     $('.li_grp_item').removeClass('languageTrHighlight');
@@ -8,6 +14,8 @@ $('.li_grp_A').off('click', '.li_grp_item').on('click', '.li_grp_item', function
           $this.removeClass("languageTrHighlight");
           }
 });
+
+//IDENTIFYING THE CLICK EVENT ON SELECTION OF RECORD FROM LIST-B
 $('.li_grp_B').off('click', '.li_grp_item').on('click', '.li_grp_item', function (event) {
     var $this = $(this);
     $('.li_grp_item').removeClass('languageTrHighlight');
@@ -17,13 +25,13 @@ $('.li_grp_B').off('click', '.li_grp_item').on('click', '.li_grp_item', function
           $this.removeClass("languageTrHighlight");
           }
 });
-//IDENTIFYING THE CLICK EVENT OF "LANGUAGE UP"
+//IDENTIFYING THE CLICK EVENT ON "Push to List B"
 $('.btn_pushToB').off('click').on('click', function (event) {
     //Element that needs to be Pushed Up
     var reqTr = $('.li_grp_A .languageTrHighlight');
     $('.li_grp_B').append(reqTr);
 });
-//IDENTIFYING THE CLICK EVENT OF "LANGUAGE DOWN"
+//IDENTIFYING THE CLICK EVENT ON "Push to List A"
 $('.btn_pushToA').off('click').on('click', function (event) {
     //Element that needs to be Pushed Up
     var reqTr = $('.li_grp_B .languageTrHighlight');
